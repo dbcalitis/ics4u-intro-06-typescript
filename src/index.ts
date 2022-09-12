e485210d0eb1c6cc013831a7f4703c791fe6da84
dbcalitis <daria.bernice.calitis@mths.ca>
2022-09-12 21:33:14 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  Daria Bernice Calitis
+ * @version 1.0
+ * @since   2022-09-12
+ */
+
+console.log('Hello, World!')
+
+console.log('\nDone.')
